6e3a1c233c8c6b2f542cddd9036e3bb4d1ffb725
HayatoNabetani <nabe_baseball1111@yahoo.co.jp>
2022-03-09 18:31:44 +0900

    参考のcolorを行いました。

--- task0/lib/main.dart
+++ task0/lib/main.dart
@@ -41,26 +41,8 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: TextStyle(
-                color: Colors.red,
-              )
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Container(
+        color: Colors.green,
       ),
     );
   }
