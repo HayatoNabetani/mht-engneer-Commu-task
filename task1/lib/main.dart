a2426244748166d6718da07cbe6fef74dd0c8702
HayatoNabetani <nabe_baseball1111@yahoo.co.jp>
2022-03-16 12:25:23 +0900

    Formで囲ってvalidationのことも考慮しました。

--- task1/lib/main.dart
+++ task1/lib/main.dart
@@ -39,112 +39,124 @@ class HexColor extends Color {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     final double deviceHeight = MediaQuery.of(context).size.height;
     final double deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: HexColor("#5000B7"),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(40, 150, 40, 10),
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: deviceWidth,
-              padding: EdgeInsets.fromLTRB(0, 20, 0, 80),
-              child: const Text(
-                "your\nportfolio",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 32,
-                  color: Colors.white,
-                ),
-              )
-            ),
-
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: TextFormField(
-                obscureText:false,
-                autocorrect:true,
-                enableInteractiveSelection:true,
-                keyboardType:TextInputType.emailAddress,
-                style: const TextStyle(
-                  color: Colors.black,
-                ),
-                decoration: InputDecoration(
-                  hintText: 'Email address',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
-              )
-            ),
-
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 5, 0, 10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: TextFormField(
-                obscureText:true,
-                autocorrect:false,
-                enableInteractiveSelection:false,
-                keyboardType:TextInputType.visiblePassword,
-                style: const TextStyle(
-                  color: Colors.black,
-                ),
-                decoration: InputDecoration(
-                  hintText: 'password',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
-              )
-            ),
-
-            Container(
-              width: deviceWidth,
-              child: const Text(
-                "Forget your password?",
-                textAlign: TextAlign.end,
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              )
-            ),
-
-            Container(
-              margin:const EdgeInsets.fromLTRB(0, 40, 0, 0),
-              width: deviceWidth,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: HexColor("FF473A"), //ボタンの背景色
-                  padding: const EdgeInsets.fromLTRB(10,20,10,20)
-                ),
+      body: Form(
+        key:_formKey,
+        child:Padding(
+          padding: EdgeInsets.fromLTRB(40, 150, 40, 10),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: deviceWidth,
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 80),
                 child: const Text(
-                  'ログイン',
+                  "your\nportfolio",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                    color: Colors.white,
+                  ),
+                )
+              ),
+
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: TextFormField(
+                  obscureText:false,
+                  autocorrect:true,
+                  enableInteractiveSelection:true,
+                  keyboardType:TextInputType.emailAddress,
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Email address',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                )
+              ),
+
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 5, 0, 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: TextFormField(
+                  obscureText:true,
+                  autocorrect:false,
+                  enableInteractiveSelection:false,
+                  keyboardType:TextInputType.visiblePassword,
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  validator: (value) {
+                    // TODO: ここで取得したtextを使う
+                    if (value == null || value.length < 5) {
+                      return '５文字より少ないです';
+                    }
+                    return null;
+                  },
+                )
+              ),
+
+              Container(
+                width: deviceWidth,
+                child: const Text(
+                  "Forget your password?",
+                  textAlign: TextAlign.end,
                   style: TextStyle(
                     color: Colors.white,
                   ),
+                )
+              ),
+
+              Container(
+                margin:const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                width: deviceWidth,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
                 ),
-                onPressed: () {
-                  // TODO: ここにフォーカスするためのコードを書く
-                },
-              )
-            ),
-            
-          ]
-        ),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: HexColor("FF473A"), //ボタンの背景色
+                    padding: const EdgeInsets.fromLTRB(10,20,10,20)
+                  ),
+                  child: const Text(
+                    'ログイン',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {
+                    _formKey.currentState!.validate();
+                  },
+                )
+              ),
+              
+            ]
+          ),
+        )
       )
     );
   }
